81b752a5756c552ad5dc05f2c0c5412e1cd73517
mandala7777 <105830381+mandala7777@users.noreply.github.com>
2022-05-21 04:53:05 -0700

    title changed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express=require('express');
-const bodyParser=require('body-parser')
 const ejs=require('ejs');
-const nodemailer=require('nodemailer')
+
 
 
 const app=express();
@@ -59,43 +58,43 @@ app.get('/appointment', (req, res)=>{
     
 })
 app.post('/send', (req, res)=>{
-    const output= `
-    <p> you have new contact request </p>
-    <h3>contact details</h3>
-    <ul>
-    <li>Name: ${req.body.name}</li>
-    <li>Email: ${req.body.email}</li>
-    <li>subject: ${req.body.subject}</li>    
-    </ul>
-    <h3>Messages</h3>
-    <p> ${req.body.message}</p>
-    `;
-    let transporter=nodemailer.createTransport({
-        host:'http://mail.mandala-group.agency:2079',
-        port:2079,
-        secure:false,
-        auth:{
-            user:'info@mandala-group.agency',
-            password:'12_Thflo0R'
-        },
-        tls:{
-            rejectUnauthorized:false
-        }
-    })
-    let mailOptions={
-        from:'"website contact" <info@mandala-group.agency>',
-        to:'info@mandala-group.agency',
-        subject:'website-contact',
-        Text:'Hello Mandala Group Inc.',
-        html:output
-    }
-    transporter.sendMail(mailOptions, (error, info)=>{
-        if(error){
-            return console.log(error)
-        }
-        console.log('Message sent: %s', info.messageId)
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
-    })
+    // const output= `
+    // <p> you have new contact request </p>
+    // <h3>contact details</h3>
+    // <ul>
+    // <li>Name: ${req.body.name}</li>
+    // <li>Email: ${req.body.email}</li>
+    // <li>subject: ${req.body.subject}</li>    
+    // </ul>
+    // <h3>Messages</h3>
+    // <p> ${req.body.message}</p>
+    // `;
+    // let transporter=nodemailer.createTransport({
+    //     host:'http://mail.mandala-group.agency:2079',
+    //     port:2079,
+    //     secure:false,
+    //     auth:{
+    //         user:'info@mandala-group.agency',
+    //         password:'12_Thflo0R'
+    //     },
+    //     tls:{
+    //         rejectUnauthorized:false
+    //     }
+    // })
+    // let mailOptions={
+    //     from:'"website contact" <info@mandala-group.agency>',
+    //     to:'info@mandala-group.agency',
+    //     subject:'website-contact',
+    //     Text:'Hello Mandala Group Inc.',
+    //     html:output
+    // }
+    // transporter.sendMail(mailOptions, (error, info)=>{
+    //     if(error){
+    //         return console.log(error)
+    //     }
+    //     console.log('Message sent: %s', info.messageId)
+    //     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+    // })
     res.render('home')
 })
 app.listen(PORT,()=>console.log(`server is up on port: ${PORT}`))
